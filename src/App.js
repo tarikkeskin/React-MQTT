6c5ede56e532320c278baa2f9f67ba8c292e088d
tarikkeskin <tarik.keskin007@gmail.com>
2021-11-25 14:30:57 +0300

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
+import {View, StyleSheet, Text, Button} from 'react-native'
+import MQTTConnection from './src/MQTTConnection'
+import { Buffer } from 'buffer';
+global.Buffer = Buffer; 
 
 function App() {
+
+  useEffect(() => {
+    this.mqttConnect = new MQTTConnection()
+    this.mqttConnect.onMQTTConnect = this.onMQTTConnect
+    this.mqttConnect.onMQTTLost = this.onMQTTLost
+    this.mqttConnect.onMQTTMessageArrived = this.onMQTTMessageArrived
+    this.mqttConnect.onMQTTMessageDelivered = this.onMQTTMessageDelivered
+
+    this.mqttConnect.connect('14.161.36.97', 1889)
+
+    onMQTTConnect = () => {
+        console.log('App onMQTTConnect')
+        this.mqttConnect.subscribeChannel('hanth2')
+    }
+
+    onMQTTLost = () => {
+        console.log('App onMQTTLost')
+    }
+
+    onMQTTMessageArrived = (message) => {
+        console.log('App onMQTTMessageArrived: ', message);
+        console.log('App onMQTTMessageArrived payloadString: ', message.payloadString);
+    }
+
+    onMQTTMessageDelivered = (message) => {
+        console.log('App onMQTTMessageDelivered: ', message);
+    }
+
+    return () => {
+      this.mqttConnect.close()
+    }
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <View style={styles.container}>
+      <Text>react_native_mqtt</Text>
+      <Button
+        title="Press me"
+        onPress={() => this.mqttConnect.send('hanth2', "message send to channel hanth2 again")}
+      />
+    </View>
+  )
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default App;
